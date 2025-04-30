0ac84c3e6a07ea2c79050b5b3d12d7d1529ee92e
brandonviaje <brandon.viaje@ontariotechu.net>
2025-04-30 19:51:38 -0400

    refactor: fix indentation and code formatting for fooditem.jsx

--- frontend/yumdrop/src/components/FoodItem/FoodItem.jsx
+++ frontend/yumdrop/src/components/FoodItem/FoodItem.jsx
@@ -4,13 +4,11 @@ import { Link } from 'react-router-dom'
 const FoodItem = ({name,description, id, imageUrl, price}) => {
   return (
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4 d-flex justify-content-center">
-    <Link to={`/food/${id}`} className="card" style={{ "maxWidth": "320px", "textDecoration": "none"}} >
-      <img
-        src={imageUrl} height={300} width={60}
-        className="card-img-top"
-        alt="Product Image"
-      />
-      <div className="card-body">
+    <div className="card" style={{ "maxWidth": "320px"}} >
+      <Link to={`/food/${id}`} >
+      <img src={imageUrl} height={300} width={60}className="card-img-top" alt="Product Image"/>
+      </Link>
+      <div className="card-body" >
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
         <div className="d-flex justify-content-between align-items-center">
@@ -27,11 +25,9 @@ const FoodItem = ({name,description, id, imageUrl, price}) => {
       </div>
       <div className="card-footer d-flex justify-content-between bg-light">
         <Link className="btn btn-primary btn-sm" to={`/food/${id}`}>View Food</Link>
-        <button className="btn btn-outline-secondary btn-sm">
-          <i className="bi bi-heart"></i>
-        </button>
+        <button className="btn btn-primary btn-sm"><i class="bi bi-plus"></i></button>
       </div>
-    </Link>
+    </div>
   </div>
   )
 }
