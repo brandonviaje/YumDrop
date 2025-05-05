ad79df81a6e0714e7649513401c17a5e5a77826c
brandonviaje <brandon.viaje@ontariotechu.net>
2025-05-05 16:39:40 -0400

    chore : rename imports

--- frontend/yumdrop/src/pages/Home/Home.jsx
+++ frontend/yumdrop/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import Header from "../../components/Header/Header";
-import ExploreMenu from "../../components/MenuBar/ExploreMenu/ExploreMenu";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import { useState } from "react";
 
