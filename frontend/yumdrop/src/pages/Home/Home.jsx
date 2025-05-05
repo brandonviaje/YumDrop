29e8f82b7c739271de600bcd0d8e438d850ba82b
brandonviaje <brandon.viaje@ontariotechu.net>
2025-05-05 16:38:02 -0400

    chore : rename imports

--- frontend/yumdrop/src/pages/Home/Home.jsx
+++ frontend/yumdrop/src/pages/Home/Home.jsx
@@ -1,19 +1,18 @@
-import Header from '../../components/MenuBar/Header/Header';
-import ExploreMenu from '../../components/MenuBar/ExploreMenu/ExploreMenu';
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
-import { useState } from 'react';
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/MenuBar/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import { useState } from "react";
 
 const Home = () => {
-
-  const [category, setCategory] = useState('all');
+  const [category, setCategory] = useState("all");
 
   return (
-    <main className='container'>
-      <Header/>
-      <ExploreMenu category={category} setCategory={setCategory}/>
-      <FoodDisplay category={category} searchText={''}/>
+    <main className="container">
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} searchText={""} />
     </main>
-  )
-}
+  );
+};
 
 export default Home;
