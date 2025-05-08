093595daa226d2d1c092f0af480c115740bb4bc6
brandonviaje <brandon.viaje@ontariotechu.net>
2025-05-08 12:54:13 -0400

    feat: add front end login/register pages

--- frontend/yumdrop/src/components/MenuBar/MenuBar.jsx
+++ frontend/yumdrop/src/components/MenuBar/MenuBar.jsx
@@ -1,14 +1,14 @@
 import React, { useContext, useState } from "react";
 import "./MenuBar.css";
 import { assets } from "../../assets/assets";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext.jsx";
 
 const MenuBar = () => {
   const [active, setActive] = useState("home");
   const { quantities } = useContext(StoreContext);
   const cartItems = Object.values(quantities).filter((qty) => qty > 0).length;
-
+  const navigate = useNavigate();
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
@@ -84,8 +84,18 @@ const MenuBar = () => {
                 </span>
               </div>
             </Link>
-            <button className="btn btn-outline-primary">Login</button>
-            <button className="btn btn-outline-success">Register</button>
+            <button
+              className="btn btn-outline-primary"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </button>
+            <button
+              className="btn btn-outline-success"
+              onClick={() => navigate("/register")}
+            >
+              Register
+            </button>
           </div>
         </div>
       </div>
